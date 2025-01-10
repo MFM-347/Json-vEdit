ecf39f5558946a3471eca98f600a60c0ecd00855
Farhan Madni <madnifm347@gmail.com>
2025-01-10 16:53:49 +0000

    save

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 import JsonEditor from "./components/JsonEditor.vue";
 import vNav from "./components/vNav.vue";
-import { Analytics } from "@vercel/analytics/vue";
 </script>
 
 <template>
   <vNav />
   <JsonEditor />
-  <Analytics />
 </template>
